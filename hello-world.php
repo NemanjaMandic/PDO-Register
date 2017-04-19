464f34a4e22a77e5daa9e6adb3cf62df50535fc9
nemanjamandic <nemanjans84@gmail.com>
2017-04-19 01:13:54 +0000

    connection class

--- hello-world.php
+++ hello-world.php
@@ -4,8 +4,27 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+ require_once('includes/pdo-connect.php');
+ 
+ $sql = 'SELECT name, meaning, gender FROM names
+         ORDER BY name';
 
 ?>
 </body>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Meaning</th>
+        <th>Gender</th>
+    </tr>
+    <?php 
+     $result = $conn->query($sql);
+     while($row = $result->fetch_assoc()){
+         echo "<tr><td>". $row["name"] . "</td><td>" . $row["meaning"] . "</td><td>" . $row["gender"] . "</td></tr>";
+     }
+    ?>
+    <tr>
+        <td></td>
+    </tr>
+</table>
 </html>
